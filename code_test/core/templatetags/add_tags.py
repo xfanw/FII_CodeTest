e65d69dee89145ec39959bfecd914d6873c5f063
frank <frank.wang1@veolia.com>
2026-07-11 16:48:35 -0500

    test

--- code_test/core/templatetags/add_tags.py
+++ code_test/core/templatetags/add_tags.py
@@ -4,7 +4,6 @@ from django.template.defaulttags import register
 @register.filter
 def minus(value, arg):
     try:
-        print(value, arg)
         return int(float(value)) - int(float(arg))
     except (ValueError, ZeroDivisionError):
         return "-- --"
