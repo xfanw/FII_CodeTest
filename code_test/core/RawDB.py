6fc54cfea453dbdac92a9a209ff115b48f400ddc
Frank Wang <frank_wang@houston.foxconn.com>
2026-07-17 10:54:39 -0500

    test

--- code_test/core/RawDB.py
+++ code_test/core/RawDB.py
@@ -4,7 +4,6 @@ from django.db import connection
 def reset_index(table_name_list):
     cursor = connection.cursor()
 
-    # 2. Send the raw query
     for table_name in table_name_list:
         try:
             cursor.execute(f"Delete from sqlite_sequence WHERE name = '{table_name}';")
